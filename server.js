12995ad959d6239ef83b2d15b0893ff8f87b6c87
VT7297 <varuntyagi0702@gmail.com>
2017-08-08 06:42:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,21 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/article1',function(req,res){
+    res.send('this is article one and it will be served here')
+});
+
+app.get('/article2',function(req,res){
+    res.send('this is article one and it will be served here')
+});
+
+app.get('/article3',function(req,res){
+    res.send('this is article one and it will be served here')
 });
 
 app.get('/ui/style.css', function (req, res) {
